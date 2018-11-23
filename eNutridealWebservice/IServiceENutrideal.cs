e9efbf731dab034e7fa1d8eab38a04772d202464
5160287 <5160287@my.ipleiria.pt>
2018-11-23 17:51:28 +0000

    ficheiros do webservice

---
+++ eNutridealWebservice/IServiceENutrideal.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace eNutridealWebservice
+{
+    [ServiceContract]
+    public interface IServiceENutrideal
+    {
+        //testeeee
+
+        // GET RECEICOES
+
+        [OperationContract]
+        [WebInvoke(Method = "GET", UriTemplate = "/refeicoes")]
+        [Description("Obter todas as refeições do eNutrideal.")]
+        List<Refeicao> GetRefeicao();
+
+        [OperationContract(Name = "GetRefeicaoPorRestaurante")]
+        [WebInvoke(Method = "GET", UriTemplate = "/refeicoes/{restaurante}")]
+        [Description("Obter refeições de determinado restaurante.")]
+        List<Refeicao> GetRefeicao(string restaurante);
+
+        // ADD REFEICAO
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "/refeicao")]
+        [Description("Adicionar Refeição ao eNutrideal.")]
+        void AddRefeicao(Refeicao refeicao);
+
+        // DELETE REFEICAO
+        [OperationContract(Name = "ApagarRefeicaoPorItem")]
+        [WebInvoke(Method = "DELETE", UriTemplate = "/refeicao/{item}")]
+        [Description("Apagar refeicao por nome(item).")]
+        void DeleteRefeicao(string item);
+
+        //CALCULADORAS
+
+        //PESO IDEAL
+        [OperationContract]
+        double calcularPesoIdeal(int idade, int altura, string genero);
+
+        //CALORIAS POR DIA
+        [OperationContract]
+        double calcularCaloriasDia(int idade, string genero, int altura, double peso, string nivelAtividade);
+        //PLANO CALORICO
+
+    }
+
+
+    [DataContract]
+    public class Refeicao
+    {
+        public Refeicao(string restaurante, string item, string quantidade, string calorias)
+        {
+            this.Restaurante = restaurante;
+            this.Item = item;
+            this.Quantidade = quantidade;
+            this.Calorias = calorias;
+        }
+        [DataMember]
+        public string Restaurante { get; set; }
+        [DataMember]
+        public string Item { get; set; }
+        [DataMember]
+        public string Quantidade { get; set; }
+        [DataMember]
+        public string Calorias { get; set; }
+
+        public override string ToString()
+        {
+            string res = string.Empty;
+            res += "Restaurante: " + Restaurante + Environment.NewLine;
+            res += "Item: " + Item + Environment.NewLine;
+            res += "Quantidade: " + Quantidade + Environment.NewLine;
+            res += "Calorias: " + Calorias + Environment.NewLine;
+            return res;
+        }
+    }
+    [DataContract]
+    public class DadosPessoais
+    {
+        [DataMember]
+        public int Idade { get; set; }
+
+        [DataMember]
+        public int Altura { get; set; }
+
+        [DataMember]
+        public double Peso { get; set; }
+
+        [DataMember]
+        public string Genero { get; set; }
+
+        [DataMember]
+        public string NivelAtividade { get; set; }
+    }
+}
---
+++ eNutridealWebservice/ServiceENutrideal.svc.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Web.Hosting;
+using System.Xml;
+
+namespace eNutridealWebservice
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class ServiceENutrideal : IServiceENutrideal
+    {
+        private static string FILEPATH;
+
+        public ServiceENutrideal()
+        {
+            FILEPATH = Path.Combine(HostingEnvironment.ApplicationPhysicalPath, "App_Data",
+           "refeicoes.xml");
+        }
+        public List<Refeicao> GetRefeicao()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            List<Refeicao> refeicoes = new List<Refeicao>();
+            XmlNodeList refeicoesNodes = doc.SelectNodes("/refeicoes/refeicao");
+            foreach (XmlNode refeicaoNode in refeicoesNodes)
+            {
+                XmlNode restauranteNode = refeicaoNode.SelectSingleNode("restaurante");
+                XmlNode itemNode = refeicaoNode.SelectSingleNode("item");
+                XmlNode quantidadeNode = refeicaoNode.SelectSingleNode("quantidade");
+                XmlNode caloriasNode = refeicaoNode.SelectSingleNode("calorias");
+                Refeicao refeicao = new Refeicao(
+                restauranteNode.InnerText,
+                itemNode.InnerText,
+                quantidadeNode.InnerText,
+                caloriasNode.InnerText
+                );
+                refeicoes.Add(refeicao);
+            }
+            return refeicoes;
+        }
+        public List<Refeicao> GetRefeicao(string restaurante)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNodeList refeicaoNodes = doc.SelectNodes("/refeicoes/refeicao[restaurante='" + restaurante +
+           "']");
+            List<Refeicao> refeicoes = new List<Refeicao>();
+            foreach (XmlNode refeicaoNode in refeicaoNodes)
+            {
+
+                XmlNode restauranteNode = refeicaoNode.SelectSingleNode("restaurante");
+                XmlNode itemNode = refeicaoNode.SelectSingleNode("item");
+                XmlNode quantidadeNode = refeicaoNode.SelectSingleNode("quantidade");
+                XmlNode caloriasNode = refeicaoNode.SelectSingleNode("calorias");
+                Refeicao refeicao = new Refeicao(
+                restauranteNode.InnerText,
+                itemNode.InnerText,
+                quantidadeNode.InnerText,
+                caloriasNode.InnerText
+                );
+                refeicoes.Add(refeicao);
+            }
+            return refeicoes;
+        }
+        
+        public void AddRefeicao(Refeicao refeicao)
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNode refeicoesNode = doc.SelectSingleNode("/refeicoes");
+            XmlElement refeicaoNode = doc.CreateElement("refeicao");
+            XmlElement restauranteNode = doc.CreateElement("restaurante");
+            restauranteNode.InnerText = refeicao.Restaurante;
+            refeicaoNode.AppendChild(restauranteNode);
+            XmlElement itemNode = doc.CreateElement("item");
+            itemNode.InnerText = refeicao.Item;
+            refeicaoNode.AppendChild(itemNode);
+            XmlElement quantidadeNode = doc.CreateElement("quantidade");
+            quantidadeNode.InnerText = refeicao.Quantidade;
+            refeicaoNode.AppendChild(quantidadeNode);
+            XmlElement caloriasNode = doc.CreateElement("calorias");
+            caloriasNode.InnerText = refeicao.Calorias;
+            refeicaoNode.AppendChild(caloriasNode);
+            refeicoesNode.AppendChild(refeicaoNode);
+            doc.Save(FILEPATH);
+        }
+
+        public void DeleteRefeicao(string item) {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(FILEPATH);
+            XmlNode refeicoesNode = doc.SelectSingleNode("/refeicoes");
+            XmlNode refeicaoNode = doc.SelectSingleNode("/refeicoes/refeicao[item='" + item + "']");
+            if (refeicaoNode != null)
+            {
+                refeicoesNode.RemoveChild(refeicaoNode);
+                doc.Save(FILEPATH);
+            }
+        }
+
+        public double calcularPesoIdeal(int idade, int altura, string genero)
+        {
+            double resultado_final = 0;
+            if (genero.Equals("Masculino"))
+            {
+
+                if (altura <= 152.4)
+                {
+                    resultado_final = 52;
+                }
+                else
+                {
+                    resultado_final = 52 + (0.7480) * (altura - 152.4);
+                }
+            }
+
+            if (genero.Equals("Feminino"))
+            {
+
+                if (altura <= 152.4)
+                {
+                    resultado_final = 49;
+                }
+                else
+                {
+                    resultado_final = 49 + (0.66929) * (altura - 152.4);
+                }
+            }
+            return resultado_final;
+        }
+
+        public double calcularCaloriasDia(int idade, string genero, int altura, double peso, string nivelAtividade)
+        {
+            double resultado_incompleto = 0;
+            double resultado_final = 0;
+
+            if (genero.Equals("Masculino"))
+            {
+
+                resultado_incompleto = 10 * peso + 6.25 * altura - 5 * idade + 5;
+
+                //10xpeso +6.25 x altura – 5 x idade + 5;
+            }
+            else
+            {
+                resultado_incompleto = 10 * peso + 6.25 * altura - 5 * idade - 161;
+            }
+
+
+            if (nivelAtividade.Equals("Sedentário"))
+            {
+                resultado_final = resultado_incompleto * 1.2;
+            }
+
+            if (nivelAtividade.Equals("Ligeiramente Ativo(a)"))
+            {
+                resultado_final = resultado_incompleto * 1.375;
+            }
+
+            if (nivelAtividade.Equals("Moderadamente Ativo(a)"))
+            {
+                resultado_final = resultado_incompleto * 1.550;
+            }
+
+            if (nivelAtividade.Equals("Muito Ativo(a)"))
+            {
+                resultado_final = resultado_incompleto * 1.725;
+            }
+
+            if (nivelAtividade.Equals("Extraordinariamente Ativo(a)"))
+            {
+                resultado_final = resultado_incompleto * 1.9;
+            }
+
+            return resultado_final;
+        }
+    }
+}
